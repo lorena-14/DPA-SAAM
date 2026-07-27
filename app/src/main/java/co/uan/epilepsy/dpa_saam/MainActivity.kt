32547358ea24eca9e1a2846feb5cdda03a49ccba
Juanca3789 <Juancaclavijo2005@gmail.com>
2026-07-27 15:18:47 -0500

    Created Uan UI Kit Dependency

--- app/src/main/java/co/uan/epilepsy/dpa_saam/MainActivity.kt
+++ app/src/main/java/co/uan/epilepsy/dpa_saam/MainActivity.kt
@@ -12,13 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.uan.epilepsy.dpa_saam.ui.theme.DPASAAMTheme
+import com.uan.designsystem.uikit.theme.UanTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DPASAAMTheme {
+            UanTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
